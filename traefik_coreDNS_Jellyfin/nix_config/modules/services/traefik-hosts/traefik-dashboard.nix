b8f6dd5e880411721ccc8c9692dea9cd61e64dc8
GenieAJ <genie072017@gmail.com>
2025-09-09 12:20:52 +0300

    Add Traefik dashboard configuration
    Configure Traefik dashboard with router and TLS settings.

---
+++ traefik_coreDNS_Jellyfin/nix_config/modules/services/traefik-hosts/traefik-dashboard.nix
@@ -0,0 +1,22 @@
+{
+  routers.traefik = {
+    rule = "Host(`traefik.test.geniehome.net`)";
+    entryPoints = [ "websecure" ];
+    service = "api@internal";
+    middlewares = [
+      "default-headers"
+      "https-redirect"
+      "geniehome-ipwhitelist"
+    ];
+    tls = {
+      certResolver = "cloudflare";
+      domains = [
+        {
+          main = "*.test.geniehome.net";
+          sans = [ "test.geniehome.net" ];
+        }
+      ];
+    };
+  };
+
+  services = {}; # No external service needed; uses internal API
